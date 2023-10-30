afc597d70ee47ab3dd34d143db99e7505052400c
lennox charles <lennyaiko17@gmail.com>
2023-10-30 21:25:02 +0100

    bug fix

--- config/env/production.js
+++ config/env/production.js
@@ -391,7 +391,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
+    baseUrl: 'https://clouder-lkvb.onrender.com',
     internalEmailAddress: 'support@example.com',
 
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
