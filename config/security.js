bda6ab14f469b057b2f0d4b3a15a5aa0d9c2b5a2
Lennox Charles <lennyaiko17@gmail.com>
2023-11-27 20:20:14 +0100

    chore

--- config/security.js
+++ config/security.js
@@ -14,49 +14,48 @@
  */
 
 module.exports.security = {
-
   /***************************************************************************
-  *                                                                          *
-  * CORS is like a more modern version of JSONP-- it allows your application *
-  * to circumvent browsers' same-origin policy, so that the responses from   *
-  * your Sails app hosted on one domain (e.g. example.com) can be received   *
-  * in the client-side JavaScript code from a page you trust hosted on _some *
-  * other_ domain (e.g. trustedsite.net).                                    *
-  *                                                                          *
-  * For additional options and more information, see:                        *
-  * https://sailsjs.com/docs/concepts/security/cors                          *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * CORS is like a more modern version of JSONP-- it allows your application *
+   * to circumvent browsers' same-origin policy, so that the responses from   *
+   * your Sails app hosted on one domain (e.g. example.com) can be received   *
+   * in the client-side JavaScript code from a page you trust hosted on _some *
+   * other_ domain (e.g. trustedsite.net).                                    *
+   *                                                                          *
+   * For additional options and more information, see:                        *
+   * https://sailsjs.com/docs/concepts/security/cors                          *
+   *                                                                          *
+   ***************************************************************************/
 
   cors: {
     allRoutes: true,
-    allowOrigins:  [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'http://localhost:3002',
-      'https://clouder-dev.netlify.app',
-      'https://clouder-ten.vercel.app'
+    allowOrigins: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:3002",
+      "https://clouder-dev.netlify.app",
+      "https://clouder-ten.vercel.app",
+      "https://clouder-nu.vercel.app",
     ],
     allowCredentials: true,
-    allowRequestHeaders: 'Authorization, Content-Type, Origin, X-Requested-With, Accept',
-    allowRequestMethods: 'GET, PUT, POST, PATCH, OPTIONS, DELETE, HEAD'
+    allowRequestHeaders:
+      "Authorization, Content-Type, Origin, X-Requested-With, Accept",
+    allowRequestMethods: "GET, PUT, POST, PATCH, OPTIONS, DELETE, HEAD",
   },
 
-
   /****************************************************************************
-  *                                                                           *
-  * By default, Sails' built-in CSRF protection is disabled to facilitate     *
-  * rapid development.  But be warned!  If your Sails app will be accessed by *
-  * web browsers, you should _always_ enable CSRF protection before deploying *
-  * to production.                                                            *
-  *                                                                           *
-  * To enable CSRF protection, set this to `true`.                            *
-  *                                                                           *
-  * For more information, see:                                                *
-  * https://sailsjs.com/docs/concepts/security/csrf                           *
-  *                                                                           *
-  ****************************************************************************/
+   *                                                                           *
+   * By default, Sails' built-in CSRF protection is disabled to facilitate     *
+   * rapid development.  But be warned!  If your Sails app will be accessed by *
+   * web browsers, you should _always_ enable CSRF protection before deploying *
+   * to production.                                                            *
+   *                                                                           *
+   * To enable CSRF protection, set this to `true`.                            *
+   *                                                                           *
+   * For more information, see:                                                *
+   * https://sailsjs.com/docs/concepts/security/csrf                           *
+   *                                                                           *
+   ****************************************************************************/
 
   // csrf: false
-
 };
