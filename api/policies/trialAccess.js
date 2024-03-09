a045517e70139969e314a7a04429e1786f0175a1
lennyaiko <lennyaiko17@gmail.com>
2024-03-09 14:00:02 +0100

    progress

--- api/policies/trialAccess.js
+++ api/policies/trialAccess.js
@@ -8,6 +8,11 @@ module.exports = async function (req, res, proceed) {
     if (req.user.plan === "trial") {
       return res.status(401).json({ status: 401, message: "Unauthorized" });
     }
+
+    const userFeatures = await Features.findOne({ owner: req.user.id });
+    if (userFeatures.subscriptions <= 4) {
+      return res.status(401).json({ status: 401, message: "Unauthorized" });
+    }
   }
   proceed();
 };
