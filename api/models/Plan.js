ccc4c7998f14a117e91a4be53ef401db4c03968a
Lennox Charles <lennyaiko17@gmail.com>
2024-09-19 15:07:50 +0100

    setup for plans

---
+++ api/models/Plan.js
@@ -0,0 +1,20 @@
+/**
+ * Plan.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    name: {
+      type: "string",
+      required: true,
+    },
+
+    price: {
+      type: "string",
+      required: true,
+    },
+  },
+};
